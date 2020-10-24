3015e4b1d2f1966f64d832e93297d7290ffbd307
mooka0 <angelankao@gmail.com>
2020-10-24 12:36:12 -0700

    shop function

--- assets/game.js
+++ assets/game.js
@@ -89,7 +89,7 @@ var endGame = function () {
 
 var shop = function() {
   // ask player what they'd like to do
-  var shopOptionsPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
   );
 
    // use switch to carry out action
